fcf56529407cdb7d57ddf128c336203128f929c2
JulianForeroBernal <julianforerobernal@gmail.com>
2026-02-20 21:45:52 -0500

    ahhh

---
+++ src/Cuenta.java
@@ -0,0 +1,53 @@
+public class Cuenta {
+    // Atributos
+    private String numCuenta;
+    private String franquicia;
+    private double saldo;
+    //constructor vacio
+    public Cuenta() {
+    }
+    // constructor con todos los parametros
+    public Cuenta(String numCuenta, String franquicia, double saldo) {
+        this.numCuenta = numCuenta;
+        this.franquicia = franquicia;
+        this.saldo = saldo;
+    }
+    // getter - setter
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getFranquicia() {
+        return franquicia;
+    }
+
+    public void setFranquicia(String franquicia) {
+        this.franquicia = franquicia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    // to string
+
+
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "numCuenta='" + numCuenta + '\'' +
+                ", franquicia='" + franquicia + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+    public double depositar(double saldo){
+        return (this.saldo + saldo);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hola mundo");
-        System.out.println("repaso comando git");
-        System.out.println("Julian Forero Bernal");
-        System.out.println("python");
+    //scaner
+        Scanner teclado = new Scanner(System.in);
 
+    //objeto creado desde el constructor vacio
+    Cuenta c1 = new Cuenta();
+    c1.setNumCuenta("654654");
+    c1.setFranquicia("Mastercard");
+    c1.setSaldo(500000);
+    System.out.println(c1.toString());
+        System.out.println("nuevo saldo" + c1.depositar(111));
+    //crear un objeto con el constructor con parametros
+        Cuenta c2 = new Cuenta("132123", "Visa", 15000);
+        System.out.println(c2.toString());
+    // crear objeto con datos ingresados por el usuario
+    Cuenta c3 = new Cuenta();
+    System.out.println("ingrese numero de cuenta: ");
+    c3.setNumCuenta(teclado.next());
+    System.out.println("ingrese franquisia: ");
+    c3.setFranquicia(teclado.next());
+    System.out.println("ingrese saldo: ");
+    c3.setSaldo(teclado.nextDouble());
+    System.out.println(c3.toString());
+    //sumar el saldo de las 3 cuentas
+    double sumaSaldo = c1.getSaldo() + c2.getSaldo() + c3. getSaldo();
+    System.out.println("el saldo de las 3 cuentas suman: " + sumaSaldo);
     }
 
 }
